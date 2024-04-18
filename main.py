78afc855995cdf183e286c40513be5a19f87f90f
redbeard-92 <82227498+redbeard-92@users.noreply.github.com>
2024-04-18 16:42:15 +0300

    test1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import openpyxl
 import subprocess
 import socket
 
+
 def check_ping(ip_address):
     try:
         result = subprocess.run(['ping', '-n', '1', ip_address], capture_output=True, text=True, timeout=5)
@@ -10,19 +11,8 @@ def check_ping(ip_address):
         print(f"Ошибка при выполнении ping для {ip_address}: {e}")
         return False
 
-def get_hostname_with_nslookup(ip_address):
-    try:
-        result = subprocess.check_output(['nslookup', ip_address], stderr=subprocess.STDOUT, universal_newlines=True)
-        lines = result.split('\n')
-        for line in lines:
-            if 'Name:' in line:
-                hostname = line.split('Name:')[1].strip()
-                return hostname
-        return None
-    except subprocess.CalledProcessError:
-        return None
 
-def get_hostname_with_ping(ip_address):
+def get_hostname(ip_address):
     try:
         result = subprocess.check_output(['ping', '-a', ip_address], stderr=subprocess.STDOUT, universal_newlines=True)
         hostname = result.split(' ')[1].strip('[]')
@@ -30,31 +20,21 @@ def get_hostname_with_ping(ip_address):
     except subprocess.CalledProcessError:
         return None
 
-def detect_os(hostname):
+
+def detect_os(ip_address):
     try:
-        result = subprocess.run(['ping', '-n', '1', hostname], capture_output=True, text=True, timeout=5)
+        result = subprocess.run(['ping', '-n', '1', ip_address], capture_output=True, text=True, timeout=5)
         if result.returncode == 0:
             if "Reply" in result.stdout:
                 return "Windows"
             else:
                 return "Linux"
     except Exception as e:
-        print(f"Ошибка при определении операционной системы для хостнейма {hostname}: {e}")
+        print(f"Ошибка при определении операционной системы для IP {ip_address}: {e}")
     return "Unknown"
 
-def add_os_info_to_sheet(sheet):
-    max_row = sheet.max_row
-    os_sheet = wb.create_sheet(title="OS Info")
-    os_sheet.append(["Hostname", "OS"])
-    for row in range(2, max_row + 1):
-        hostname = sheet.cell(row=row, column=3).value
-        if not hostname:
-            hostname = sheet.cell(row=row, column=5).value
-        if hostname:
-            detected_os = detect_os(hostname)
-            os_sheet.append([hostname, detected_os])
 
-def process_hostnames(sheet):
+def process_devices(sheet):
     max_row = sheet.max_row
     for row in range(2, max_row + 1):
         ip_address = sheet.cell(row=row, column=2).value
@@ -65,25 +45,47 @@ def process_hostnames(sheet):
                     sheet.cell(row=row, column=4).value = "Доступен"
                 else:
                     sheet.cell(row=row, column=4).value = "Недоступен"
-                hostname = get_hostname_with_nslookup(ip_address)
-                if not hostname:
-                    hostname = get_hostname_with_ping(ip_address)
+
+                hostname = get_hostname(ip_address)
                 if hostname:
-                    sheet.cell(row=row, column=3).value = hostname
+                    sheet.cell(row=row, column=3).value = hostname.encode('ascii', 'ignore').decode('ascii')
                 else:
                     sheet.cell(row=row, column=3).value = "Не удалось получить хостнейм"
+
+                os = detect_os(ip_address)
+                sheet.cell(row=row, column=5).value = os
             except Exception as e:
-                print(f"Ошибка при обработке хостнейма с IP {ip_address}: {e}")
+                print(f"Ошибка при обработке устройства с IP {ip_address}: {e}")
+
+
+def process_hostnames(sheet):
+    max_row = sheet.max_row
+    for row in range(2, max_row + 1):
+        hostname = sheet.cell(row=row, column=6).value
+        if hostname:
+            print(f"Пингуется хостнейм: {hostname}")
+            try:
+                ip_address = socket.gethostbyname(hostname)
+                sheet.cell(row=row, column=7).value = ip_address
+                if check_ping(ip_address):
+                    sheet.cell(row=row, column=8).value = "Доступен"
+                else:
+                    sheet.cell(row=row, column=8).value = "Недоступен"
+                os = detect_os(ip_address)
+                sheet.cell(row=row, column=9).value = os
+            except Exception as e:
+                print(f"Ошибка при обработке хостнейма {hostname}: {e}")
+
 
 # Открываем файл hosts.xlsx
 wb = openpyxl.load_workbook('hosts.xlsx')
 sheet = wb.active
 
+# Обработка устройств
+process_devices(sheet)
+
 # Обработка хостнеймов
 process_hostnames(sheet)
-
-# Добавляем информацию об операционных системах
-add_os_info_to_sheet(sheet)
 
 # Сохраняем изменения
 wb.save('hosts.xlsx')
